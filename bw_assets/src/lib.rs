a3eb68ece8704eb300340d234e6bd1926ff404ba
Ryan <drdgvhbh@gmail.com>
2020-10-25 01:43:51 -0400

    Initialize bw assets package

---
+++ bw_assets/src/lib.rs
@@ -0,0 +1,5 @@
+//! Types and Parsers for Starcraft Broodwar Asset Formats
+//!
+//! This crate provides the types and parsers necessary to process Brood War
+//! assets. First class support is provided to load these assets using the
+//! Amethyst game engine.
